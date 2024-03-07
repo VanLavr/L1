03e7b0ca204dc065a4a8953262abe90c04d5edac
VanLavr <lavr.i-n@mail.ru>
2024-03-07 12:59:33 +0300

    added a task

--- task15CodeSample.go
+++ task15CodeSample.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+)
+
 var justString string
 
 func someFunc() {
@@ -9,4 +14,16 @@ func someFunc() {
 
 func main() {
 	someFunc()
+	fmt.Println(justString)
+}
+
+func createHugeString(n int) string {
+	const letters = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+	runes := []rune(letters)
+	buffer := make([]rune, n)
+	for b := range buffer {
+		buffer[b] = runes[rand.Intn(len(runes))]
+	}
+
+	return string(buffer)
 }
